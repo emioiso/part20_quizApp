ab47006f6b0f80e7e1d3413b33d77c5a7af4d80f
emioiso <jijjjiijiijijiji@gmail.com>
2023-11-02 13:59:58 +0700

    Initial Commit

---
+++ part20_quizApp/part20_quizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  part20_quizAppApp.swift
+//  part20_quizApp
+//
+//  Created by emi oiso on 2023/11/02.
+//
+
+import SwiftUI
+
+@main
+struct part20_quizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
